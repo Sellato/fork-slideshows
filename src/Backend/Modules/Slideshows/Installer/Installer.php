26a1e9bc19eda8b138ebf1d3bed88255460605a2
Jelmer Prins <jelmer@sumocoders.be>
2015-05-05 10:31:37 +0200

    re-add the settings after jonas explained that is is used in the backend

--- src/Backend/Modules/Slideshows/Installer/Installer.php
+++ src/Backend/Modules/Slideshows/Installer/Installer.php
@@ -54,6 +54,9 @@ class Installer extends ModuleInstaller
             'slideshows/index',
             array('slideshows/add', 'slideshows/add_slide', 'slideshows/edit', 'slideshows/edit_slide')
         );
+        $navigationSettingsId = $this->setNavigation(null, 'Settings');
+        $navigationModulesSettingsId = $this->setNavigation($navigationSettingsId, 'Modules');
+        $this->setNavigation($navigationModulesSettingsId, 'Slideshows', 'slideshows/settings');
 
         // create folders if needed
         $imagePath = FRONTEND_FILES_PATH . '/Slideshows';
