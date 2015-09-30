6c95adc383a27e0de319c5484eabb4f531a15290
Jonas De Keukelaere <jonas@sumocoders.be>
2015-09-30 11:12:18 +0100

    Remove more copyright
    This is not part of Fork CMS.

--- src/Backend/Modules/Slideshows/Ajax/Sequence.php
+++ src/Backend/Modules/Slideshows/Ajax/Sequence.php
@@ -2,13 +2,6 @@
 
 namespace Backend\Modules\Slideshows\Ajax;
 
-/*
- * This file is part of Fork CMS.
- *
- * For the full copyright and license information, please view the license
- * file that was distributed with this source code.
- */
-
 use Backend\Core\Engine\Base\AjaxAction as BackendBaseAJAXAction;
 use Backend\Modules\Slideshows\Engine\Model as BackendSlideshowsModel;
 
